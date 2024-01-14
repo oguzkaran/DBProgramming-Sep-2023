f336f1101544e8540440c9460f3d2be22a66a516
Oğuz Karan <oguzkaran@csystem.org>
2024-01-14 23:47:49 +0300

    Update

--- src/sample.sql
+++ src/sample.sql
@@ -1,34 +1,22 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Sınıf Çalışması: Aşağıdaki tabloları oluşturunuz ve istenenleri yapınız
-	- customers
-		- customer_id
-		- name
-		- address
-	phones		
-		- phone_number
-		- customer_id
-	phone_invoices
-		- id
-		- phone_number
-		- invoice_date
-		- last_pay_date
-		- paid_date (nullable)
-		- total
-	Sorular:
-		- Fatura ödeme merkezinin faturaya ilişkin bilgiler ile müşteri bilgilerinin elde edildiği bir view yazınız. 
-		Fatura ödenmişse bilgi elde edilmeyecektir. View için gerekli gizlemeleri yapınız
-
-		- Ödenmiş faturaları elde eden bir view yazınız
-
-		- Fatura ödenmemişse fatura ödeme merkezinin ödeme tamamlandığında view ile update yapabileceği bir SP yazınız
+	İçiçe try deyimlerinde içteki catch bloğunda işlemm yapıldıktan sonra tekrar aynı error'un fırlatılması için throw
+	tek başına kullanılır. Buna yeniden fırlatma (rethrow) denir. Bu durumda sentaks olarak throw'dan önce gelen deyimin
+	noktalı virgül ile sonlandırılması gerekir
 -----------------------------------------------------------------------------------------------------------------------*/
 
-create view v_older_people_all_info
-as
-select * from people where dbo.get_age(birth_date) > 65
-with check option 
+declare @val int = rand()* 20 - 10
 
-insert into v_older_people_all_info (citizen_id, first_name, last_name, birth_date, marital_status_id, gender_id) values ('12345678915', 'Fatma', 'Selami', '1940-09-13', 1, 1)
-
-select * from v_people_info
-
+begin try
+	begin try
+		if @val < 0
+			throw 67000, 'Can not be negative', 1
+		select @val
+	end try
+	begin catch
+		select 'inner catch';
+		throw --rethrow
+	end catch
+end try
+begin catch
+	select 'outer catch'
+end catch

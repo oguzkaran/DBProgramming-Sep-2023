40fe1efba84bfd51676e6cf5632fb60369de3294
Oğuz Karan <oguzkaran@csystem.org>
2024-01-19 21:21:41 +0300

    Update

--- src/sample.sql
+++ src/sample.sql
@@ -1,22 +1,65 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	İçiçe try deyimlerinde içteki catch bloğunda işlemm yapıldıktan sonra tekrar aynı error'un fırlatılması için throw
-	tek başına kullanılır. Buna yeniden fırlatma (rethrow) denir. Bu durumda sentaks olarak throw'dan önce gelen deyimin
-	noktalı virgül ile sonlandırılması gerekir
+	Sınıf Çalışması: Aşağıdaki veritabanını oluşturunuz ve soruları cevaplayınız
+	cities
+		- city_id
+		- name
+	patients
+		- patient_id
+		- full_name
+		- city_id
+		- birth_date
+	relations
+		- relation_id 
+		- description (Mother, Dad, Child, Aunt, Uncle etc)
+	companions
+		- companion_id
+		- full_name
+		- patient_id,
+		- relation_id
+	Sorular:
+	1. Tüm patient_id'lere ilişkin hastaların isimlerini büyük harfe çeviren SP'yi yazınız
+	2. Parametresi ile aldığı yaştabn büyük olan hastaların refaakçi ve kendi isimlerini büyük harfe çeviren SP'yi
+	yazınız
+	3. Parametresi ile aldığı city_id değerine göre refakatçi isimlerini küçük harfe çeviren SP'yi yazınız
 -----------------------------------------------------------------------------------------------------------------------*/
+create table staff (
+	staff_id int primary key identity(1, 1),
+	citizen_id char(40) unique not null,
+	first_name nvarchar(100) not null,
+	last_name nvarchar(100) not null,
+	birth_date date not null,
+	phone char(20) not null
+)
 
-declare @val int = rand()* 20 - 10
 
-begin try
-	begin try
-		if @val < 0
-			throw 67000, 'Can not be negative', 1
-		select @val
-	end try
-	begin catch
-		select 'inner catch';
-		throw --rethrow
-	end catch
-end try
-begin catch
-	select 'outer catch'
-end catch
+go
+
+create function get_random_staff_citizen_id()
+returns char(40)
+as
+begin
+	declare @bound int = (select count(*) from staff) + 1
+	declare @min int = 1
+
+	declare @index int = floor(rand() * (@bound - @min) + @min)
+	declare crs_staff cursor scroll for select citizen_id from staff 
+	open crs_staff 
+
+	declare @citizen_id char(40) = ''
+
+	fetch absolute @index from crs_staff into @citizen_id
+
+	close crs_staff
+	deallocate crs_staff
+
+	return @citizen_id
+end
+
+go
+
+
+declare @citizen_id char(40)
+
+exec sp_fill_random_staff @citizen_id output
+
+select * from staff where citizen_id = @citizen_id
